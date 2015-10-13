709a1ac45ef1550b33b3b94f8f2ef3df45ec3dc2
Efimov Maxim <maxefi@mail.ru>
2015-10-13 14:56:46 +0300

    gulp redone + enter done

--- gulpfile.js
+++ gulpfile.js
@@ -1,37 +1,39 @@
+// ██╗   ██╗ █████╗ ██████╗ ███████╗
+// ██║   ██║██╔══██╗██╔══██╗██╔════╝
+// ██║   ██║███████║██████╔╝███████╗
+// ╚██╗ ██╔╝██╔══██║██╔══██╗╚════██║
+//  ╚████╔╝ ██║  ██║██║  ██║███████║
+//   ╚═══╝  ╚═╝  ╚═╝╚═╝  ╚═╝╚══════╝
+
 var gulp = require("gulp"),
-	minifyCSS = require('gulp-minify-css'),
-	rename = require('gulp-rename'),
-	notify = require('gulp-notify'),
-	autoprefixer = require('gulp-autoprefixer'),
-	browserSync = require('browser-sync');
+		browserSync = require('browser-sync'),
+		del = require("del"),
+        gutil = require("gulp-util"),
+		concatCSS = require('gulp-concat-css'),
+		minifyCSS = require('gulp-minify-css'),
+		uglify = require('gulp-uglify'),
+		gulpif = require("gulp-if"),
+		autoprefixer = require("gulp-autoprefixer"),
+		ftp = require("vinyl-ftp"),
+		wiredep = require("wiredep").stream,
+		size = require("gulp-size"),
+		notify = require('gulp-notify');
 
 
 
-/*CSS task - css, watchcss*/
-gulp.task('css', function () {
-  gulp.src('app/css/*.css')
-    .pipe(minifyCSS({compatibility: 'ie8'}))
-    .pipe(rename('main.min.css'))
-    .pipe(autoprefixer('last 2 versions', '> 1%', 'ie 8'))
-    .pipe(gulp.dest('app/dist/'))
-    .pipe(notify({
-    	'title': 'CSS',
-    	'message': "optimized"
-	}));
-});
-
-gulp.task('watchcss', function () {
-	gulp.watch('app/css/main.css', ['css']);
-})
-
-
+//  █████╗ ██████╗ ██████╗
+// ██╔══██╗██╔══██╗██╔══██╗
+// ███████║██████╔╝██████╔╝
+// ██╔══██║██╔═══╝ ██╔═══╝
+// ██║  ██║██║     ██║
+// ╚═╝  ╚═╝╚═╝     ╚═╝
 
 /*BROWSERSYNC task - server, watch*/
 gulp.task('server', function () {
-	browserSync({
+	browserSync.init({
 		port: 9000,
 		server: {
-			baseDir: 'app'
+			baseDir: "./app"
 		}
 	});
 });
@@ -44,4 +46,152 @@ gulp.task('watch', function () {
 	]).on('change', browserSync.reload);
 });
 
-gulp.task('default', ['server', 'watch', 'watchcss']);
+// CONNECTING BOWER TASK - wiredep-bower
+gulp.task("wiredep-bower", function () {
+    return gulp.src("./app/*.html")
+        .pipe(wiredep({
+            directory: "./app/bower",
+            /*, overrides: {
+                "qtip2": {
+                    "main": ["./jquery.qtip.min.js", "./jquery.qtip.min.css"],
+                    "dependencies": {"jquery": ">=1.6.0"}
+                }
+            }*/
+            //, exclude: ["bower/qtip2/"]
+            // , ignorePath: /^(\.\.\/)*\.\./
+        }))
+        .pipe(gulp.dest("./app"));
+});
+
+gulp.task("watch-bower", function () {
+    return gulp.watch("watch-bower", ["wiredep-bower"]);
+});
+
+// DEFAULT TASK
+gulp.task("default", ["watch", "server", "wiredep-bower", "watch-bower"]);
+
+var log = function (error) {
+    console.log([
+        "",
+        "----------ERROR MESSAGE START----------",
+        ("[" + error.name + " in " + error.plugin + "]"),
+        error.message,
+        "----------ERROR MESSAGE END----------",
+        ""
+    ].join("\n"));
+    this.end();
+}
+
+
+
+// ██████╗ ██╗███████╗████████╗
+// ██╔══██╗██║██╔════╝╚══██╔══╝
+// ██║  ██║██║███████╗   ██║
+// ██║  ██║██║╚════██║   ██║
+// ██████╔╝██║███████║   ██║
+// ╚═════╝ ╚═╝╚══════╝   ╚═╝
+
+// TRANSFER
+// HTML TASK - t-html
+gulp.task("t-html", function () {
+    return gulp.src("./app/*.html")
+        .pipe(gulp.dest("dist"))
+        .pipe(notify({
+    	'title': 'HTML',
+    	'message': "TRANSFERED TO DIST"
+	}));
+});
+
+// CSS TASK - t-css
+gulp.task("t-css", function () {
+    return gulp.src("./app/css/*.css")
+    	.pipe(concatCSS("main.min.css"))
+        .pipe(gulpif("*.css", minifyCSS({compatibility: "ie8"})))
+        .pipe(autoprefixer('last 2 versions', '> 1%', 'ie 8'))
+        .pipe(gulp.dest("dist/css"))
+        .pipe(notify({
+    	'title': 'CSS',
+    	'message': "TRANSFERED TO DIST"
+	}));
+});
+
+// JS TASK - t-js
+gulp.task("t-js", function () {
+    return gulp.src("./app/js/*.js")
+        .pipe(gulpif("*.js", uglify()))
+        .pipe(gulp.dest("dist/js"))
+        .pipe(notify({
+    	'title': 'JS',
+    	'message': "TRANSFERED TO DIST"
+	}));
+});
+
+//BOWER TASK - t-bower
+gulp.task("t-bower", function() {
+	return gulp.src("./app/bower/**/*.+(js|css)")
+		.pipe(gulp.dest("./dist/bower/"))
+});
+
+//FONTS TASK - t-fonts
+gulp.task("t-fonts", function() {
+    return gulp.src("./app/fonts/**")
+    	.pipe(gulpif("*.css", minifyCSS({compatibility: "ie8"})))
+        .pipe(gulp.dest("./dist/fonts/"))
+});
+
+//FAVICON TASK - t-favi
+gulp.task("t-favi", function () {
+	return gulp.src("./app/favicon/*")
+		.pipe(gulp.dest("./dist/favicon"))
+});
+
+// CLEAN DIST TASK - clean-dist
+gulp.task("clean-dist", function () {
+    return del(["./dist/**", "!./dist"])
+});
+
+//DIST SERVER TASK - server-dist
+gulp.task("server-dist", function () {
+    browserSync.init({
+        port: 8000,
+        server: { baseDir: "./dist" }
+    });
+});
+
+
+
+// ██████╗ ██╗   ██╗██╗██╗     ██████╗ ███████╗
+// ██╔══██╗██║   ██║██║██║     ██╔══██╗██╔════╝
+// ██████╔╝██║   ██║██║██║     ██║  ██║███████╗
+// ██╔══██╗██║   ██║██║██║     ██║  ██║╚════██║
+// ██████╔╝╚██████╔╝██║███████╗██████╔╝███████║
+// ╚═════╝  ╚═════╝ ╚═╝╚══════╝╚═════╝ ╚══════╝
+
+//APP SIZE TASK - size-app
+gulp.task("size-app", function () {
+    return gulp.src("app/**/*").pipe(size({title: "APP size: "}));
+});
+
+//DIST BUILD TASK - dist
+gulp.task("dist", ["t-html", "t-css", "t-js", "t-bower", "t-fonts", "t-favi", "size-app"], function () {
+    return gulp.src("dist/**/*").pipe(size({title: "DIST size: "}));
+});
+
+//DIST ON READY BUILD TASK - build
+gulp.task("build", ["clean-dist", "wiredep-bower"], function () {
+    gulp.start("dist");
+});
+
+// Отправка проекта на сервер
+gulp.task("deploy", function() {
+    var conn = ftp.create({
+        host: "host",
+        user: "user",
+        password: "password",
+        parallel: 10,
+        log: gutil.log
+    });
+
+    return gulp.src(["./dist/**/*"], { base: "./dist/", buffer: false})
+            .pipe(conn.dest("/public_html/"));
+});

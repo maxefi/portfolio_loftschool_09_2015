21643e735566bf550b1ad1e42c3ea62513c97fe5
Efimov Maxim <maxefi@mail.ru>
2015-12-22 03:05:21 +0300

    Php part1 done

--- config/config.php
+++ config/config.php
@@ -6,5 +6,11 @@ return array(
         'host' => 'localhost',
         'user' => 'root',
         'password' => '',
-        'dbname' => 'portfolio')
+        'dbname' => 'portfolio'),
+    'captcha' => array(
+        'public_key' => '6LeVnhMTAAAAAD-EoZ8nmmGS8Mwkj_mBFn3PEvwd',
+        'secret_key' => '6LeVnhMTAAAAAI4fLln1Zs-34HwA_oMHSRCmw5ZU',
+        'url_check_request' => 'https://www.google.com/recaptcha/api/siteverify',
+        'js_script' => "https://www.google.com/recaptcha/api.js"
+     )
 );
